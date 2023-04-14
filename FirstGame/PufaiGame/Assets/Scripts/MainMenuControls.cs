da096f80db3fca50d5632dfb107233f4ab8adb79
Ilya <shubin152@gmail.com>
2023-04-14 16:48:11 +0500

    Дополнил меню настроек
    Сделал рабочий переключатель fullscreen, начал настройку ползунка громкости, начал настройку выбора разрешения

---
+++ FirstGame/PufaiGame/Assets/Scripts/MainMenuControls.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class MainMenuControls : MonoBehaviour
+{   
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Levels");
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+
+        Debug.Log("Exit pressed!");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    //Тут начинается меню настроек
+
+    //Настройка полного экрана
+    public void FullScreenToggle()
+    {
+        //fullscreen;
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+
+
+    //надо сделать настройку громкости музыки через slider "volume"
+    //public GameSettings am;
+    public void SetAudioVolume(float sliderValue)
+    {
+        //masterVolume это параметр для панели громкости
+        //раскомментировать когда разберемся с 
+        //am.SetFloat("masterVolume", sliderValue);
+    }
+}
