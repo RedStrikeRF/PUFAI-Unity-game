429567b13b4749cdf8d7606ca8c39d72a3e2aa38
Kdzefuf <115563854+Kdzefuf@users.noreply.github.com>
2023-04-25 21:33:42 +0500

    Движение объекта
    Добавил движение объекта после введения координат движения

--- FirstGame/PufaiGame/Assets/Scripts/CannonShoot.cs
+++ FirstGame/PufaiGame/Assets/Scripts/CannonShoot.cs
@@ -7,25 +7,35 @@ public class CannonShoot : MonoBehaviour
     public float speed = 3;
     private int press = 0;
     public TMP_InputField user;
-
-    public void SetVector()
+    public bool IsPressed;
+    public Vector2 SetVector()
     {
         string XX = user.text;
         var coordinate = XX.Split(',');
         direction.x = float.Parse(coordinate[0]);
         direction.y = float.Parse(coordinate[1]);
-        Debug.Log(direction.x);
+        return direction;
     }
 
-    public void ButtonPress()
+    public void SetVectors()
     {
-        press += 1;
-        Debug.Log(press);
+        Debug.Log(SetVector());
+    }
+
+    public void ButtonPressed()
+    {
+        IsPressed = true;
+    }
+
+    public void GetTarget()
+    {
+        SetVector();
+        transform.Translate(direction * speed);
     }
 
     void Update()
     {
-        Debug.Log("gg");
-        transform.Translate(direction * speed);
+        if (IsPressed)
+            GetTarget();
     }
 }
