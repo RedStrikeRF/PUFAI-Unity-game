dd01e3edb052276648d0579330cc40ae3a743a1e
Kdzefuf <115563854+Kdzefuf@users.noreply.github.com>
2023-05-16 00:08:06 +0500

    Изменение первого уровня

--- FirstGame/PufaiGame/Assets/Scripts/CannonShoot.cs
+++ FirstGame/PufaiGame/Assets/Scripts/CannonShoot.cs
@@ -1,19 +1,34 @@
+using System.Threading;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CannonShoot : MonoBehaviour
 {
+    public bool IsPressed;
     public Vector2 direction;
     public float speed = 3;
     private int press = 0;
     public TMP_InputField user;
-    public bool IsPressed;
+    public TextMeshProUGUI loseText;
+    public Button startButton;
+
+    void Start()
+    {
+        IsPressed = false;
+    }
+
     public Vector2 SetVector()
     {
         string XX = user.text;
         var coordinate = XX.Split(',');
         direction.x = float.Parse(coordinate[0]);
         direction.y = float.Parse(coordinate[1]);
+        if (XX != "0,-1")
+        {
+            Thread.Sleep(1000);
+            Lose();
+        }
         return direction;
     }
 
@@ -24,7 +39,10 @@ public class CannonShoot : MonoBehaviour
 
     public void ButtonPressed()
     {
-        IsPressed = true;
+        if (startButton.isActiveAndEnabled)
+        {
+            IsPressed = true;
+        }
     }
 
     public void GetTarget()
@@ -33,9 +51,13 @@ public class CannonShoot : MonoBehaviour
         transform.Translate(direction * speed);
     }
 
+    public void Lose()
+    {
+        loseText.gameObject.SetActive(true);
+    }
+
     void Update()
     {
-        if (IsPressed)
-            GetTarget();
+        startButton.Invoke("GetTarget", 0);
     }
 }
