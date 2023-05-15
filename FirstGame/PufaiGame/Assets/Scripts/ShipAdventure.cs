8ab9c96ed99fd2ca3c89fb928250810800eb4048
Kdzefuf <115563854+Kdzefuf@users.noreply.github.com>
2023-05-16 02:41:15 +0500

    Ограничения координат и обновление уровня
    Добавили ограничение движение корабля по уровню и обновление уровня

--- FirstGame/PufaiGame/Assets/Scripts/ShipAdventure.cs
+++ FirstGame/PufaiGame/Assets/Scripts/ShipAdventure.cs
@@ -1,25 +1,34 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ShipAdventure : MonoBehaviour
 {
     public bool IsPressed = false;
     public Vector2 direction;
-    public float speed = 3;
+    private int targetPosition = 250;
+    private int maxY = 950;
+    private int minX = 550;
+    private int maxX = 1650;
+    private float speed = 3;
+    private static int waitSeconds = 100;
+    public WaitForSeconds wait;
     public TMP_InputField user;
     public TextMeshProUGUI loseText;
     public Button startButton;
 
     void Update()
-    {   
-        transform.Translate(direction * speed);
+    {
+        if (transform.position.y >= targetPosition && transform.position.y <= maxY
+            && transform.position.x >= minX && transform.position.x <= maxX)
+            transform.Translate(direction * speed);
     }
 
     public void Lose()
     {
         loseText.gameObject.SetActive(true);
-
+        SceneManager.LoadScene("FirstLevel");
     }
 
     public Vector2 SetVector()
@@ -41,5 +50,11 @@ public class ShipAdventure : MonoBehaviour
         IsPressed = true;
         Debug.Log(SetVector());
         SetVector();
+    }
+
+    private WaitForSeconds Wait()
+    {
+        wait = new WaitForSeconds(waitSeconds);
+        return wait;
     }
 }
