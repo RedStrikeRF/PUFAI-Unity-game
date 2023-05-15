f4ad352689c9a22bb9ca2bb5be991348213e3376
Ilya <shubin152@gmail.com>
2023-05-16 03:20:40 +0500

    SecondLevel
    Сделали основу 2-го уровня, добавили  движение, варитивность для ответов

---
+++ FirstGame/PufaiGame/Assets/Scripts/PirateAttack.cs
@@ -0,0 +1,62 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PirateAttack : MonoBehaviour
+{
+    public bool IsPressed = false;
+    public Vector2 direction;
+    private int targetPosition = 250;
+    private int maxY = 950;
+    private int minX = 550;
+    private int maxX = 1650;
+    private float speed = 3;
+    private static int waitSeconds = 100;
+    public WaitForSeconds wait;
+    public TMP_InputField user;
+    public TextMeshProUGUI loseText;
+    public Button startButton;
+
+    void Update()
+    {
+        if (transform.position.y >= targetPosition && transform.position.y <= maxY
+            && transform.position.x >= minX && transform.position.x <= maxX)
+            transform.Translate(direction * speed);
+    }
+
+    public void Lose()
+    {
+        loseText.gameObject.SetActive(true);
+        SceneManager.LoadScene("SecondLevel");
+    }
+
+    public Vector2 SetVector()
+    {
+        string XX = user.text;
+        var coordinate = XX.Split(',');
+        direction.x = float.Parse(coordinate[0]);
+        direction.y = float.Parse(coordinate[1]);
+        if (XX != "-3,3")
+        {
+            Lose();
+        }
+        direction.x += 6;
+        direction.y -= 3;
+        return direction;
+    }
+
+    //здесь start
+    public void ButtonPressed()
+    {
+        IsPressed = true;
+        Debug.Log(SetVector());
+        SetVector();
+    }
+
+    private WaitForSeconds Wait()
+    {
+        wait = new WaitForSeconds(waitSeconds);
+        return wait;
+    }
+}
